8b6edbf65f7a35d1a82898c76784f34dbbf37d67
Timur Kalimullin <timur_kalimullin@list.ru>
2022-01-06 19:12:27 +0500

    add launch json

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello, NextJS!</div>;
+  const q = "asdf";
+  return <div>Hello, NextJS! {q}</div>;
 }
